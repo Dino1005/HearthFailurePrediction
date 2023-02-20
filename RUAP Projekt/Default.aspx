6dd46d4526d4817cf82ca9a7cdb3e81af6d1b3eb
Dino1005 <radonjicdino@gmail.com>
2023-02-20 17:36:47 +0100

    Initial commit

---
+++ RUAP Projekt/Default.aspx
@@ -0,0 +1,160 @@
+﻿<%@ Page Language="C#" MaintainScrollPositionOnPostback="true" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RUAP_Projekt.Default" %>
+
+<%@ Register Assembly="System.Web.DataVisualization, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35" Namespace="System.Web.UI.DataVisualization.Charting" TagPrefix="asp" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Hearth Failure Prediction</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <div class="wrapper">
+        <div class="title">
+            HEARTH FAILURE PREDICTION
+        </div>
+
+        <form runat="server">
+            <div class="container">
+                <div class="left">
+                    <div class="input_field">
+                        <label>Age:</label>
+                        <asp:TextBox ID="txtAge" CssClass="input" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="input_field">
+                        <label>Anaemia:</label>
+                        <asp:RadioButtonList ID="rblAnaemia" CssClass="input_radio" RepeatDirection="Horizontal" runat="server">
+                            <asp:ListItem Value="1">Yes</asp:ListItem>
+                            <asp:ListItem Value="0">No</asp:ListItem>
+                        </asp:RadioButtonList>
+                    </div>
+                    <div class="input_field">
+                        <label>Creatinine phosphokinase:</label>
+                        <asp:TextBox ID="txtCreatinine" CssClass="input" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="input_field">
+                        <label>Diabetes:</label>
+                        <asp:RadioButtonList ID="rblDiabetes" CssClass="input_radio" RepeatDirection="Horizontal" runat="server">
+                            <asp:ListItem Value="1">Yes</asp:ListItem>
+                            <asp:ListItem Value="0">No</asp:ListItem>
+                        </asp:RadioButtonList>
+                    </div>
+                    <div class="input_field">
+                        <label>Ejection fraction:</label>
+                        <asp:TextBox ID="txtEjection" CssClass="input" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="input_field">
+                        <label>High blood pressure:</label>
+                        <asp:RadioButtonList ID="rblBloodPressure" CssClass="input_radio" RepeatDirection="Horizontal" runat="server">
+                            <asp:ListItem Value="1">Yes</asp:ListItem>
+                            <asp:ListItem Value="0">No</asp:ListItem>
+                        </asp:RadioButtonList>
+                    </div>
+                </div>
+                <div class="right">
+                    <div class="input_field">
+                        <label>Platelets:</label>
+                        <asp:TextBox ID="txtPlatelets" CssClass="input" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="input_field">
+                        <label>Serum creatinine:</label>
+                        <asp:TextBox ID="txtSerumC" CssClass="input" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="input_field">
+                        <label>Serum sodium:</label>
+                        <asp:TextBox ID="txtSerumS" CssClass="input" runat="server"></asp:TextBox>
+                    </div>
+                    <div class="input_field">
+                        <label>Sex:</label>
+                        <asp:RadioButtonList ID="rblSex" CssClass="input_radio" RepeatDirection="Horizontal" runat="server">
+                            <asp:ListItem Value="1">Male</asp:ListItem>
+                            <asp:ListItem Value="0">Female</asp:ListItem>
+                        </asp:RadioButtonList>
+                    </div>
+                    <div class="input_field">
+                        <label>Smoking:</label>
+                        <asp:RadioButtonList ID="rblSmoking" CssClass="input_radio" RepeatDirection="Horizontal" runat="server">
+                            <asp:ListItem Value="1">Yes</asp:ListItem>
+                            <asp:ListItem Value="0">No</asp:ListItem>
+                        </asp:RadioButtonList>
+                    </div>
+                    <div class="input_field">
+                        <label>Follow-up period:</label>
+                        <asp:TextBox ID="txtTime" CssClass="input" runat="server"></asp:TextBox>
+                    </div>
+                </div>
+            </div>
+            <div class="bottom">
+                <asp:Label ID="lblText" CssClass="text" runat="server"></asp:Label>
+                <div class="input_field">
+                    <label>Choose an algorithm:</label>
+                    <asp:DropDownList ID="ddlAlgorithm" CssClass="input" runat="server">
+                        <asp:ListItem Value="0">Neural network</asp:ListItem>
+                        <asp:ListItem Value="1">Support vector machine</asp:ListItem>
+                        <asp:ListItem Value="2">Locally-deep support vector machine</asp:ListItem>
+                        <asp:ListItem Value="3">Bayes point machine</asp:ListItem>
+                        <asp:ListItem Value="4">Boosted decision tree</asp:ListItem>
+                        <asp:ListItem Value="5">Decision forest</asp:ListItem>
+                        <asp:ListItem Value="6">Decision jungle</asp:ListItem>
+                        <asp:ListItem Value="7">Logistic regression</asp:ListItem>
+                        <asp:ListItem Value="8">Averaged perceptron</asp:ListItem>
+                    </asp:DropDownList>
+                </div>
+                <div class="input_field">
+                    <asp:Button ID="Submit" CssClass="button" runat="server" Text="SUBMIT" OnClick="Submit_Click"/> 
+                </div>
+            </div>
+            <hr/>
+            <div class="chart">
+                <div class="chart_header">
+                    <p class="subtitle">Visualizating deaths by parameters</p>
+                    <div class="input_field">
+                        <label>Select a variable:</label>
+                        <asp:DropDownList ID="ddlVariable" CssClass="input" AutoPostBack="true" runat="server">
+                            <asp:ListItem Value="0">Age</asp:ListItem>
+                            <asp:ListItem Value="1">Anaemia</asp:ListItem>
+                            <asp:ListItem Value="2">Diabetes</asp:ListItem>
+                            <asp:ListItem Value="3">Ejection fraction</asp:ListItem>
+                            <asp:ListItem Value="4">High blood pressure</asp:ListItem>
+                            <asp:ListItem Value="5">Serum creatinine</asp:ListItem>
+                            <asp:ListItem Value="6">Serum sodium</asp:ListItem>
+                            <asp:ListItem Value="7">Sex</asp:ListItem>
+                            <asp:ListItem Value="8">Smoking</asp:ListItem>
+                        </asp:DropDownList>
+                    </div>
+                </div>
+                <div class="chart_body">
+                    <canvas class="pie" id="pie"></canvas>
+                </div>
+                <asp:Button ID="btnReset" CssClass="reset" runat="server" Text="RESET" OnClick="Reset_Click"/>
+            </div>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js"></script>
+            <script type="text/javascript"> 
+                var config = {
+                    type: 'pie',
+                    data: {
+                        labels: ['',''],
+                        datasets: [{
+                            data: [0, 0],
+                            backgroundColor: ["red", "blue", "green"]
+                        }]
+                    },
+                };
+
+                window.onload = function () {
+                    var pie = document.getElementById("pie").getContext('2d');
+                    var chartData = '<%=data%>';
+                    var chartLabel = '<%=label%>';
+                    config.data.datasets.forEach(function (dataset) {
+                        dataset.data = chartData.split(' ');
+                    });
+                    config.data.labels = chartLabel.split(' ');
+                    window.myPie = new Chart(pie, config);
+                }
+            </script>
+
+        </form>
+    </div>
+</body>
+</html>
---
+++ RUAP Projekt/Default.aspx.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using RestSharp;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace RUAP_Projekt
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        string apiKey, apiLink, probability, prediction;
+
+        static int anaemiaYesCount = 0, anaemiaNoCount = 0, diabetesYesCount = 0, diabetesNoCount = 0, pressureYesCount = 0, pressureNoCount = 0, sexMaleCount = 0, sexFemaleCount = 0, smokingYesCount = 0, smokingNoCount = 0;
+        static List<int> ages = new List<int>();
+        static List<int> ejections = new List<int>();
+        static List<double> serumCs = new List<double>();
+        static List<int> serumSs = new List<int>();
+
+        public static string data = "0 0";
+        public static string label = "YES NO";
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ChangeVariable();
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+
+            if (IsFieldEmpty())
+                lblText.Text = "Required field is empty!";
+            else
+            {
+                lblText.Text = "";
+                var options = new RestClientOptions()
+                {
+                    MaxTimeout = -1,
+                };
+                var client = new RestClient(options);
+
+                GetLinkAndKey(ddlAlgorithm.SelectedValue);
+                var request = new RestRequest(apiLink, Method.Post);
+                request.AddHeader("Authorization", apiKey);
+                request.AddHeader("Content-Type", "application/json");
+                var body = "{\"Inputs\":{\"input1\":{\"ColumnNames\":[\"age\",\"anaemia\",\"creatinine_phosphokinase\",\"diabetes\",\"ejection_fraction\",\"high_blood_pressure\",\"platelets\",\"serum_creatinine\",\"serum_sodium\",\"sex\",\"smoking\",\"time\",\"DEATH_EVENT\"],\"Values\":[[\"" + txtAge.Text + "\",\"" + rblAnaemia.SelectedValue + "\",\"" + txtCreatinine.Text + "\",\"" + rblDiabetes.SelectedValue + "\",\"" + txtEjection.Text + "\",\"" + rblBloodPressure.SelectedValue + "\",\"" + txtPlatelets.Text + "\",\"" + txtSerumC.Text + "\",\"" + txtSerumS.Text + "\",\"" + rblSex.SelectedValue + "\",\"" + rblSmoking.SelectedValue + "\",\"" + txtTime.Text + "\",\"0\"]]}},\"GlobalParameters\":{}}";
+                request.AddStringBody(body, DataFormat.Json);
+
+                ParseOutput(client.Execute(request));
+
+                string result = prediction == "1" ? "likely" : "not likely";
+                lblText.Text = $"Patient is {result} to suffer hearth failure. Scored probability is: {probability}";
+            }
+
+        }
+
+        protected bool IsFieldEmpty()
+        {
+            return string.IsNullOrEmpty(txtAge.Text) || string.IsNullOrEmpty(rblAnaemia.SelectedValue) || string.IsNullOrEmpty(txtCreatinine.Text)
+                || string.IsNullOrEmpty(rblDiabetes.SelectedValue) || string.IsNullOrEmpty(txtEjection.Text) || string.IsNullOrEmpty(rblBloodPressure.SelectedValue)
+                || string.IsNullOrEmpty(txtPlatelets.Text) || string.IsNullOrEmpty(txtSerumC.Text) || string.IsNullOrEmpty(txtSerumS.Text)
+                || string.IsNullOrEmpty(rblSex.SelectedValue) || string.IsNullOrEmpty(rblSmoking.SelectedValue) || string.IsNullOrEmpty(txtTime.Text);
+        }
+
+        protected void ParseOutput(RestResponse response)
+        {
+            var results = JObject.Parse(response.Content);
+            var values = results["Results"]["output1"]["value"]["Values"].Last();
+
+            probability = Convert.ToDecimal(values.Last().ToString(), CultureInfo.InvariantCulture).ToString("#0.##%");
+            prediction = values.ElementAt(values.Count() - 2).ToString();
+
+            if (string.Equals(prediction, "1"))
+            {
+                if (values[1].ToString() == "1")
+                    anaemiaYesCount++;
+                else
+                    anaemiaNoCount++;
+
+                if (values[3].ToString() == "1")
+                    diabetesYesCount++;
+                else
+                    diabetesNoCount++;
+
+                if (values[5].ToString() == "1")
+                    pressureYesCount++;
+                else
+                    pressureNoCount++;
+
+                if (values[9].ToString() == "1")
+                    sexMaleCount++;
+                else
+                    sexFemaleCount++;
+
+                if (values[10].ToString() == "1")
+                    smokingYesCount++;
+                else
+                    smokingNoCount++;
+
+                ages.Add(Convert.ToInt32(values[0]));
+                ejections.Add(Convert.ToInt32(values[4]));
+                serumCs.Add(Convert.ToDouble(values[7]));
+                serumSs.Add(Convert.ToInt32(values[8]));
+            }
+
+            ChangeVariable();
+        }
+
+        protected void GetLinkAndKey(string algorithm)
+        {
+            switch (algorithm)
+            {
+                case "0":
+                    apiLink = "https://ussouthcentral.services.azureml.net/workspaces/f4139a912af043b2af070ea9ee0a7c79/services/3b2b250d84874d3a932d65daf307ea5a/execute?api-version=2.0&details=true";
+                    apiKey = "Bearer EK9XIESGnDoxamdrzROK+ujaeAZmphmRZwqIEKFpEBmdc60Y3kDmH9gacKcF2/IeTUQq7SdnsaHU+AMCU4ezAg==";
+                    break;
+
+                case "1":
+                    apiLink = "https://ussouthcentral.services.azureml.net/workspaces/f4139a912af043b2af070ea9ee0a7c79/services/da7d741526df498ba6b62b204773db54/execute?api-version=2.0&details=true";
+                    apiKey = "Bearer SJNmchEZBNwT4npsTPYd185YuIBQoPAaQLZn3NWYbJzJTjmlv/I9GG7L8pJbrPT1zxSWopw5aQFu+AMCajtawg==";
+                    break;
+
+                case "2":
+                    apiLink = "https://ussouthcentral.services.azureml.net/workspaces/f4139a912af043b2af070ea9ee0a7c79/services/50e95bd203094573971eb0abf9056b39/execute?api-version=2.0&details=true";
+                    apiKey = "Bearer qYzYPJMw66Jqb8Mw1CXoiFWgMgQOXSAe+ADjRKTxP3ssvtu7aLTuPG8S8HesoWnF/lIfAqHHlcWD+AMC/iZ6CA==";
+                    break;
+
+                case "3":
+                    apiLink = "https://ussouthcentral.services.azureml.net/workspaces/f4139a912af043b2af070ea9ee0a7c79/services/973bc5be1cfa458396b574d8158e779c/execute?api-version=2.0&details=true";
+                    apiKey = "Bearer F4NF0Np4l4zdpr/pztHVsYdrX0ikFWVt/lJVc2ItQ9pomJSvvweLPwHUOGgah4/0mWpqPPUgsSzQ+AMC6JYV0A==";
+                    break;
+
+                case "4":
+                    apiLink = "https://ussouthcentral.services.azureml.net/workspaces/f4139a912af043b2af070ea9ee0a7c79/services/1d125756e7d04576b4275f348246349e/execute?api-version=2.0&details=true";
+                    apiKey = "Bearer U/dwrkoALhg5kOY0tt2809YmBfuWx/+jQ3TFznvtC8D2DJcEuFd4ictbt4A9g27dMF3GqPMzkB2i+AMC4wH+mA==";
+                    break;
+
+                case "5":
+                    apiLink = "https://ussouthcentral.services.azureml.net/workspaces/f4139a912af043b2af070ea9ee0a7c79/services/bf4651b9073f4f8f9c94f8b963ce4166/execute?api-version=2.0&details=true";
+                    apiKey = "Bearer 5d5VIfXJXtzoAAmBMoIXPEAAxitBMrr/XWEbcwd++XTVfpS7nDhXqwkDZjoK0ISwlrWrgQfu702w+AMCc6JKOA==";
+                    break;
+
+                case "6":
+                    apiLink = "https://ussouthcentral.services.azureml.net/workspaces/f4139a912af043b2af070ea9ee0a7c79/services/66e45daa14be4345868f16d5ac47697e/execute?api-version=2.0&details=true";
+                    apiKey = "Bearer J5g20SaLbVJxXi8PqfG6ALCMiaqbRclL7Lbt25imxJy00QMn/lUtlsLeVzjtGQ8eUwBn0OoTj1D1+AMCyjfEVw==";
+                    break;
+
+                case "7":
+                    apiLink = "https://ussouthcentral.services.azureml.net/workspaces/f4139a912af043b2af070ea9ee0a7c79/services/30cfa26d952b4a859e84a0f5d25c67a4/execute?api-version=2.0&details=true";
+                    apiKey = "Bearer PaMTfX/wNgd72O+Mn6G0DGfmp5PGTJTBRzTAGd7fometTITqVhJ+Ihpolw8hVybueagUKiIJZkuj+AMCLJWeDQ==";
+                    break;
+
+                case "8":
+                    apiLink = "https://ussouthcentral.services.azureml.net/workspaces/f4139a912af043b2af070ea9ee0a7c79/services/84fcf3b9fd514e8c9b2653e59490b72b/execute?api-version=2.0&details=true";
+                    apiKey = "Bearer b1sF15K2Yf16+RsmL5GvlO2+znxVOhERRyBiWyZxT7gj+cZFXUD5gbMVU5dF6OLJC28ClBHGVBuD+AMCIpAHCw==";
+                    break;
+            }
+        }
+
+        protected void ChangeVariable()
+        {
+            int value1, value2, value3;
+            if (ddlVariable.SelectedValue == "0")
+            {
+                value1 = ages.Where(x => x < 40).Count();
+                value2 = ages.Where(x => x >= 40 && x <= 60).Count();
+                value3 = ages.Where(x => x > 60).Count();
+                data = $"{value1} {value2} {value3}";
+                label = "<40 40-60 >60";
+            }
+            else if (ddlVariable.SelectedValue == "1")
+            {
+                data = $"{anaemiaYesCount} {anaemiaNoCount}";
+                label = "YES NO";
+            }
+            else if (ddlVariable.SelectedValue == "2")
+            {
+                data = $"{diabetesYesCount} {diabetesNoCount}";
+                label = "YES NO";
+            }
+            else if (ddlVariable.SelectedValue == "3")
+            {
+                value1 = ejections.Where(x => x < 30).Count();
+                value2 = ejections.Where(x => x >= 30 && x <= 40).Count();
+                value3 = ejections.Where(x => x > 40).Count();
+                data = $"{value1} {value2} {value3}";
+                label = "<30 30-40 >40";
+            }
+            else if (ddlVariable.SelectedValue == "4")
+            {
+                data = $"{pressureYesCount} {pressureNoCount}";
+                label = "YES NO";
+            }
+            else if (ddlVariable.SelectedValue == "5")
+            {
+                value1 = serumCs.Where(x => x < 1).Count();
+                value2 = serumCs.Where(x => x >= 1 && x <= 2).Count();
+                value3 = serumCs.Where(x => x > 2).Count();
+                data = $"{value1} {value2} {value3}";
+                label = "<1 1-2 >2";
+            }
+            else if (ddlVariable.SelectedValue == "6")
+            {
+                value1 = serumSs.Where(x => x < 130).Count();
+                value2 = serumSs.Where(x => x >= 130 && x <= 140).Count();
+                value3 = serumSs.Where(x => x > 140).Count();
+                data = $"{value1} {value2} {value3}";
+                label = "<130 130-140 >140";
+            }
+            else if (ddlVariable.SelectedValue == "7")
+            {
+                data = $"{sexMaleCount} {sexFemaleCount}";
+                label = "MALE FEMALE";
+            }
+            else if (ddlVariable.SelectedValue == "8")
+            {
+                data = $"{smokingYesCount} {smokingNoCount}";
+                label = "YES NO";
+            }
+        }
+
+        protected void Reset_Click(object sender, EventArgs e)
+        {
+            anaemiaYesCount = 0;
+            anaemiaNoCount = 0;
+            diabetesYesCount = 0;
+            diabetesNoCount = 0;
+            pressureYesCount = 0;
+            pressureNoCount = 0;
+            sexMaleCount = 0;
+            sexFemaleCount = 0;
+            smokingYesCount = 0;
+            smokingNoCount = 0;
+            ages.Clear();
+            ejections.Clear();
+            serumCs.Clear();
+            serumSs.Clear();
+
+            ChangeVariable();
+        }
+    }
+}
